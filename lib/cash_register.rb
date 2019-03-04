5d909eb40d59d132bf68e00375ea99fc138e3510
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-04 16:53:40 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,45 @@
 
+class CashRegister
+  #creates readers/writers for total and discount
+  attr_accessor :total, :discount, :recent_items
+  
+#initializes an register with total of zero and an optional employee discount
+def initialize(discount = nil)
+  @total = 0
+  @discount = discount
+  @instance_items = []
+end 
+
+#adds an item by updating total price and items array
+def add_item(title, price, quantity = 1)
+  @total += price*quantity
+  @instance_items << [title, price, quantity]
+end
+
+ #applies employee discount if appropriate      
+def apply_discount
+if @discount != nil 
+  @total -= (@total*@discount/100)
+  "After the discount, the total comes to $#{@total}."
+else
+  "There is no discount to apply."
+end
+end
+
+#tells you what items you put into your register
+def items
+  instance_item_display = []
+  @instance_items.each do |entry|
+    quantity = entry[2]
+    item = entry[0]
+       quantity.times{instance_item_display << item} 
+  end 
+    instance_item_display
+end 
+
+def void_last_transaction
+price = @instance_items[-1][1]
+@total -= price
+end 
+
+end 
