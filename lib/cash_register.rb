38547ea80db2f67bb0ab3d2476530d158244f9bf
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-04 16:49:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,7 +4,7 @@ class CashRegister
   attr_accessor :total, :discount, :recent_items
   
 #creates an array of all items so you know what you chose  
-@@all_items = []
+@@class_items = []
   
 #initializes an register with total of zero and an optional employee discount
 def initialize(discount = nil)
